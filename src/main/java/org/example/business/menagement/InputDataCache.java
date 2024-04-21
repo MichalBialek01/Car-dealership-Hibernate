af4aa8df5ec688f110bfc107d87578d5413ada52
Michał Białek <kontakt.michalbialek@gmail.com>
2024-04-22 00:00:19 +0200

    Saving inputData to database - finding misspelling. Actually completely works.

--- src/main/java/org/example/business/menagement/InputDataCache.java
+++ src/main/java/org/example/business/menagement/InputDataCache.java
@@ -5,7 +5,6 @@ import lombok.experimental.UtilityClass;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -14,11 +13,9 @@ import java.util.stream.Collectors;
 
 @UtilityClass
 public class InputDataCache {
-    private static final String FILE_PATH = "./src/main/resources/trafic_simulation.md";
-
+    private static final String FILE_PATH = "./src/main/resources/traffic_simulation.md";
 
     private static final Map<String, List<String>> inputData;
-
 
     static {
         try {
@@ -29,20 +26,19 @@ public class InputDataCache {
     }
 
     private static Map<String, List<String>> readFileContent() throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH))
-                .stream()
-                .filter(line -> line.startsWith("[//]: #"))
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH)).stream()
+                .filter(line -> !line.startsWith("[//]: #"))
                 .filter(line -> !line.isBlank())
                 .toList();
+
         return lines.stream()
                 .collect(Collectors.groupingBy(
                         line -> line.split("->")[0].trim(),
                         Collectors.mapping(
-                                line -> line.substring(line.indexOf("->") + 1).trim(),
+                                line -> line.substring(line.indexOf("->") + 2).trim(),
                                 Collectors.toList()
                         )
                 ));
-
     }
 
     public static <T> List<T> getInputData(
@@ -54,6 +50,17 @@ public class InputDataCache {
                 .orElse(List.of())
                 .stream()
                 .filter(line -> line.startsWith(entity.toString()))
+                .map(mapper)
+                .toList();
+    }
+
+    public static <T> List<T> getInputData(
+            final Keys.InputDataGroup inputDataGroup,
+            final Function<String, T> mapper
+    ) {
+        return Optional.ofNullable(inputData.get(inputDataGroup.toString()))
+                .orElse(List.of())
+                .stream()
                 .map(mapper)
                 .toList();
     }
